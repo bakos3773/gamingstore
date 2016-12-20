9dbd3092a97b6ccb7bb018f4cf7d3ebb87697bd2
Bakos <p.bakowski1@gmail.com>
2016-12-20 02:04:59 +0100

    start

---
+++ src/main/java/com/bakos/GamingstoreApplication.java
@@ -0,0 +1,12 @@
+package com.bakos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingstoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bakos/GamingstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bakos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamingstoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
